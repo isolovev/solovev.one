9e5b59614a82b039103c396c30600ea510430ceb
Ivan Solovev <ivan@solovev.one>
2019-02-11 02:50:20 +0300

    Added the dictionary

---
+++ src/utils/Dictionary.js
@@ -0,0 +1,25 @@
+const _dictionary = {
+  ru: {
+    headings: {
+      about: "Обо мне",
+      projects: "Мои работы",
+    },
+
+    linkOnProject: "Ссылка на проект",
+    goToHome: "Вернуться на главную страницу",
+
+    siteTitle: "Иван Соловьев – Frontend разработчик",
+    info: {
+      main: "Привет, я – Ваня",
+      specialty: "Frontend разработчик",
+    },
+
+    pageError: "Нет такой страницы",
+  },
+};
+
+export const Dictionary = (() => {
+  const lang = "ru";
+
+  return _dictionary[lang];
+})();
