44d8d66c5c281af86ec22f8bfc76275f9847636d
Ivan Solovev <ivan@solovev.one>
2019-03-08 15:49:10 +0300

    Added the transition for the projects page

--- src/components/Projects/Projects.ts
+++ src/components/Projects/Projects.ts
@@ -1,18 +1,41 @@
 import Draft, { DraftItem } from "../Draft/Draft";
 import { getElement } from "../../utils/getElements";
 import request from "../../utils/request";
+import { getTransitionTime } from "../../utils/getTransitionTime";
 
 class Projects {
-	private static list = getElement(".projects__list");
+	private root = getElement(".projects");
+	private wrap = getElement(".projects__wrap");
+	private list = getElement(".projects__list");
+
+	private timeTransition = getTransitionTime(this.root);
 
 	constructor() {
 		request(`/project-list`)
 			.then(this.createList);
 	}
 
+	public show(): void {
+		this.root.hidden = false;
+
+		setTimeout(() => {
+			this.root.classList.add("projects--show");
+			this.wrap.classList.add("projects__wrap--show");
+		}, 1);
+	}
+
+	public hide(): void {
+		this.root.classList.remove("projects--show");
+		this.wrap.classList.remove("projects__wrap--show");
+
+		setTimeout(() => {
+			this.root.hidden = true;
+		}, this.timeTransition);
+	}
+
 	private createList = (items: DraftItem[]): void => {
 		items.forEach((item: DraftItem) => {
-			Projects.list.appendChild(Draft.render(item));
+			this.list.appendChild(Draft.render(item));
 		});
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,22 @@ import Projects from "./components/Projects/Projects";
 
 const projects = new Projects();
 
-// import routeNames from "./utils/routeNames";
-//
-// route(routeNames.home, () => {
-// 	Home.show();
-// });
-//
-// route(routeNames.about, () => {
-// 	Home.hide();
-// });
-//
-// route(routeNames.projects, () => {
-// 	Home.hide();
-// });
-//
-// route.base("/");
-// route.start(true);
-//
-// document.body.addEventListener("click", (event) => {
-// 	// @ts-ignore
-// 	const link: HTMLLinkElement = event.target.closest("a");
-// 	if (link && link.href.match(window.location.origin)) {
-// 		event.preventDefault();
-// 		route(link.getAttribute("href"));
-// 	}
-// });
+route("/", () => {
+	projects.hide();
+});
+
+route("projects/", () => {
+	projects.show();
+});
+
+route.base("/");
+route.start(true);
+
+document.body.addEventListener("click", (event) => {
+	// @ts-ignore
+	const link: HTMLLinkElement = event.target.closest("a");
+	if (link && link.href.match(window.location.origin)) {
+		event.preventDefault();
+		route(link.getAttribute("href"));
+	}
+});
