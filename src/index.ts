86c2f0c628f6f36744eb2457da638d133c550b3f
Ivan Solovev <ivan@solovev.one>
2019-03-23 05:44:49 +0300

    Change router

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,38 @@
 // @ts-ignore
 import "intersection-observer";
-// @ts-ignore
-import route from "riot-route";
+
+import Router from "./components/Router/Router";
 
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 
+const siteTitle = `Иван Соловьев – Frontend разработчик`;
+
 const home = new Home();
 const projects = new Projects();
 
-route("/", () => {
-	home.init();
-	projects
-		.hide()
-		.then(home.show);
-});
+const router = new Router([
+	{
+		url: "/",
+		title: siteTitle,
+		callback: () => {
+			home.init();
+			projects
+				.hide()
+				.then(home.show);
+		},
+	},
+	{
+		url: "/projects",
+		title: `Мои работы. ${siteTitle}`,
+		callback: () => {
+			home.hide();
+			projects.show();
+		},
+	},
+]);
 
-route("projects", () => {
-	home.hide();
-	projects.show();
-});
-
-route.base("/");
-route.start(true);
+router.listen();
 
 document.body.addEventListener("click", (event) => {
 	// @ts-ignore
@@ -34,6 +44,6 @@ document.body.addEventListener("click", (event) => {
 			top: 0,
 		});
 
-		route(link.getAttribute("href"));
+		router.navigate(link.getAttribute("href"));
 	}
 });
